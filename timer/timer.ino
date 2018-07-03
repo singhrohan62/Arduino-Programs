9200f1f1fa944937d031e1581c56b0b0bf6ae583
Rohan Singh <rohaniiitv@gmail.com>
2018-07-03 20:05:44 +0530

    added comments in the arduino file

--- timer/timer.ino
+++ timer/timer.ino
@@ -1,14 +1,21 @@
 int ledPin = 13;
-  
+
 void setup() {
   // put your setup code here, to run once:
+
   pinMode(ledPin,OUTPUT);
+  //Setting pin no. 13 to be o/p pin
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   digitalWrite(ledPin,HIGH);
   delay(1000);
+  //Setting pin no. 13 to be in ON state and delaying for 1 sec(~1000 ms)
+
+
   digitalWrite(ledPin,LOW);
   delay(1000);
+  //Setting pin no. 13 to be in OFF state and delaying for 1 sec(~1000 ms)
 }
